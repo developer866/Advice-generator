eb3d58f986adbaa7175910c80d03c23ab9a44952
Ayeni opeyemi [factorial] <66707128+developer866@users.noreply.github.com>
2022-07-31 00:36:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+async function adviceApi() {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
+  Id(data.slip);
+  adviceText(data.slip);
+}
+
+adviceApi();
+const adviceId = document.getElementById("AdviceId");
+const AdviceText = document.getElementById("AdviceText");
+const Dice = document.getElementById("dice");
+
+function Id(num) {
+  adviceId.innerHTML = `<h1>Adivce #${num.id}</h1>`;
+}
+function adviceText(text) {
+  AdviceText.innerHTML = `<p>"${text.advice}"</p>`;
+}
+Dice.addEventListener("click", function () {
+  adviceApi();
+  console.log("hello world");
+});
